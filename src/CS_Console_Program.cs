5e926754ed9d4fafd6a9c170441946295b11521d
magec101 <magec101@manipalglobal.com>
2015-01-27 10:36:39 +0530

    Project saved in IDE @Tue Jan 27 10:36:35 2015 1

---
+++ src/CS_Console_Program.cs
@@ -0,0 +1,10 @@
+using System;
+ 
+namespace LearnCsharp
+{
+	class CS_Console_Program{
+	    public static void Main(string[] args) {
+			Console.Write("Program passed " + args.Length + " arguments");
+	    }
+	}
+}
